859f1ef00713fc0c76be5b258c65cc6ec1aceea3
Студент1 <1@DESKTOP-7RO4MU5>
2022-12-01 11:12:25 +0300

    Добавьте файлы проекта.

---
+++ TrainProject/Check.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainProject;
+
+public static class Check
+{
+    public static void Likes(string[] name)
+    {
+        if (name is null)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Никто не оценил");
+            Console.WriteLine();
+            return;
+        }
+        else if (name.Length == 1)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{name[0]} оценил запись");
+            Console.WriteLine();
+            return;
+        }
+        else if (name.Length == 2)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{name[0]} and {name[1]} оценили запись");
+            Console.WriteLine();
+            return;
+        }
+        else if (name.Length == 3)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{name[0]}, {name[1]} and {name[2]} оценили запись");
+            Console.WriteLine();
+            return;
+        }
+        else if (name.Length >= 4)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{name[0]}, {name[1]} and {name.Length - 2} others оценили запись");
+            Console.WriteLine();
+            return;
+        }
+    }
+}
---
+++ TrainProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using TrainProject;
+void Likes()
+{
+    Console.WriteLine("Введите имена людей");
+    string selectNames = Console.ReadLine();
+    bool check = selectNames.All(Char.IsDigit);
+    if (selectNames == "")
+    {
+        string[] names = selectNames.Split(new char[] { ' ' });
+        names = null;
+        Check.Likes(names);
+    }
+    else if (selectNames.First() == ' ')
+    {
+        string[] names = selectNames.Split(new char[] { ' ' });
+        names = null;
+        Check.Likes(names);
+    }
+    else
+    {
+        string[] names = selectNames.Split(new char[] { ' ' });
+        Check.Likes(names);
+    }
+}
+
+while (true)
+{
+    Likes();
+}
